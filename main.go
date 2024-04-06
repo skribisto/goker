a52888ded0618d9594f16124a66b84d0c7afb165
skribisto <mull@dene.ch>
2024-04-06 21:11:52 +0200

    feat (scores,cards,main): working scoring system
    functioning scoring system for a given set of cards
    expects 5 cards + 2 from player, as it's texas hold'em

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"goker/cards"
+	"goker/scores"
 	"log"
 	"os"
 )
@@ -24,20 +25,63 @@ func main() {
 
 	log.SetOutput(logFile)
 
-	log.Println("#####################################")
+	//log.Println("#####################################")
 	log.Println("############# New Game   ############")
-	log.Println("#####################################")
+	//log.Println("#####################################")
 
-	var d cards.Deck
-	d = d.New()
+	d := cards.NewDeck()
+
+	var board0 cards.Board
+	var board1 cards.Board
+	var board2 cards.Board
 
 	// Deal cards from the deck
+	for i := 0; i < 5; i++ {
+		card, err := d.Deal()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		board0 = append(board0, card)
+	}
+	log.Println("Dealt board cards: ", board0)
 	for i := 0; i < 2; i++ {
 		card, err := d.Deal()
 		if err != nil {
 			log.Fatalln(err)
 		}
-		log.Println("Dealt card: ", card)
+		board1 = append(board1, card)
+	}
+	log.Println("Dealt p1 cards: ", board1)
+	for i := 0; i < 2; i++ {
+		card, err := d.Deal()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		board2 = append(board2, card)
+	}
+	log.Println("Dealt p2 card: ", board2)
+	board1 = append(board1, board0...)
+	board2 = append(board2, board0...)
+
+	sc1, err := scores.Score(board1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sc2, err := scores.Score(board2)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	i, err := scores.CompareScoreCards(sc1, sc2)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if i == -1 {
+		log.Println("player 2 won")
+	} else if i == 1 {
+		log.Println("player 1 won")
+	} else {
+		log.Println("ex aequo")
 	}
 
 	/*

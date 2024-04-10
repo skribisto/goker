ac5a9d7e5a3a684015c82279bbb925ea45e9aa11
skribisto <mull@dene.ch>
2024-04-10 15:44:36 +0200

    fix (play,game): proper cli
    working questioning
    introducting AI dumb behavior
    nicer printing of gameplay

--- cmd/game.go
+++ cmd/game.go
@@ -6,137 +6,133 @@ import (
 	"goker/pkg/log"
 )
 
-/*
-func deal(players int) []cards.Hand {
-	hands := make([]cards.Hand, players, players)
-	for p := 0; p < players; p++ {
-		hands[p].Deal(2)
-	}
-	return hands
-}
-*/
-
 func Execute() {
-	p0, err := plays.NewPlayer("Skrib")
+	var answer string
+	rageQuit := false
+	p0, err := plays.NewPlayer("Skrib", 0)
+	if err != nil {
+		log.Fatalf("could not instantiate Player0 : %w", err)
+	}
+
+	p1, err := plays.NewPlayer("CPU1", 1)
 	if err != nil {
 		log.Fatalf("could not instantiate Player1 : %w", err)
 	}
 
-	p1, err := plays.NewPlayer("Computer1")
+	p2, err := plays.NewPlayer("CPU2", 1)
 	if err != nil {
-		log.Fatalf("could not instantiate Player2 : %w", err)
+		log.Fatalf("could not instantiate Player1 : %w", err)
 	}
-
+	log.Debugf("%v is sitting on the bench", p2.Name)
+	//players := []*plays.Player{p0, p1, p2}
 	players := []*plays.Player{p0, p1}
 
-	p, err := plays.NewPlay(players)
-	if err != nil {
-		log.Fatalf("%w", err)
-	}
-	log.Infof("BlindInfos : %v", p.Blinds)
-
-	var answer string
-	rageQuit := false
 	for !rageQuit {
-		log.Info("#####################################")
-		log.Info("############ P0's turn  #############")
-		log.Info("#####################################")
 
-		//time.Sleep(500 * time.Millisecond)
+		p, err := plays.NewPlay(players)
+		if err != nil {
+			log.Fatalf("%w", err)
+		}
+		log.Infof("BlindInfos : %v", p.Blinds)
+
 		for p.Round != 4 {
+			currentRount := p.Round
 			if err := p.BeginRound(); err != nil {
 				log.Fatal("round could not begin")
 			}
+			for id := range p.Players {
+				if !p.Players[id].StillPlays {
+					continue
+				}
+				if p.Round == 0 && len((*p.Bets)[0]) == 2 {
+					if len(p.Players) > 2 && id < 2 {
+						log.Debug(">2 is a crowd, skip 2 players due to blinds")
+						continue
+					} else if id == 0 {
+						log.Debug("begining new play in heads up, skip small blind player")
+						continue
+					}
+				}
 
-			cards, err := p.GetPlayerCards(p0.ID)
-			if err != nil {
-				log.Fatalf("%w", err)
-			}
-			log.Infof("Player %v has: %v", p0.Name, *cards)
+				log.Info("#####################################")
+				log.Infof("######### P%v: %v's turn  ############", id, p.Players[id].Name)
+				log.Info("#####################################")
 
-			log.Infof("Bets for this round are: %v", (*p.Bets)[p.Round])
+				isAutonomous := false
+				if p.Players[id].Strategy == 1 {
+					log.Info("Player is autonomous")
+					isAutonomous = true
+				}
 
-			canCheck, err := p.CanCheck(p0.ID)
-			if err != nil {
-				log.Fatalf("got error during CanCheck %w", err)
-			}
-			if canCheck {
-				log.Info("Player0, What do you want to do ? (bet/check/fold)")
-			} else {
-				log.Info("Player0, What do you want to do ? (bet/fold)")
-			}
-
-			//read input from user
-			fmt.Scanln(&answer)
-			switch answer {
-			case "bet":
-				if err := p.PutBet(p0.ID, p.Blinds.SmallBlind); err != nil {
+				cards, err := p.GetPlayerCards(id)
+				if err != nil {
 					log.Fatalf("%w", err)
 				}
-			case "check":
-				if err := p.PutBet(p0.ID, 0); err != nil {
+
+				board, err := p.GetBoard()
+				if err != nil {
 					log.Fatalf("%w", err)
 				}
-			case "fold":
-				if err := p.Fold(p0.ID); err != nil {
-					log.Fatalf("%w", err)
+				if !isAutonomous {
+					log.Infof("%v has: %v          board: %v", p.Players[id].Name, *cards, *board)
+					log.Infof("Bets for this round are: %v", (*p.Bets)[p.Round])
 				}
-			default:
-				log.Info("I didn't understand")
-				continue
-			}
-			answer = ""
 
-			if p.Round == 4 {
-				break
-			}
-
-			log.Info("#######################")
-			log.Info("##### AI's turn #######")
-			log.Info("#######################")
-			//time.Sleep(500 * time.Millisecond)
-
-			cards, err = p.GetPlayerCards(p1.ID)
-			if err != nil {
-				log.Fatalf("%w", err)
-			}
-			log.Infof("AI has: %v", *cards)
-			log.Infof("Bets for this round are: %v", (*p.Bets)[p.Round])
-
-			canCheck, err = p.CanCheck(p1.ID)
-			if err != nil {
-				log.Fatalf("got error during CanCheck %w", err)
-			}
-			if canCheck {
-				log.Info("AI, What do you want to do ? (bet/check/fold)")
-			} else {
-				log.Info("AI, What do you want to do ? (bet/fold)")
-			}
-
-			//read input from user
-			fmt.Scanln(&answer)
-			switch answer {
-			case "bet":
-				if err := p.PutBet(p1.ID, p.Blinds.SmallBlind); err != nil {
-					log.Fatalf("%w", err)
+				canCheck, err := p.CanCheck(id)
+				if err != nil {
+					log.Fatalf("got error during CanCheck %w", err)
 				}
-			case "check":
-				if err := p.PutBet(p1.ID, 0); err != nil {
-					log.Fatalf("%w", err)
-				}
-			case "fold":
-				if err := p.Fold(p1.ID); err != nil {
-					log.Fatalf("%w", err)
-				}
-			default:
-				log.Info("I didn't understand")
-				continue
-			}
+				if isAutonomous {
+					if err := p.Call(id); err != nil {
+						log.Fatalf("%w", err)
+					}
+				} else {
+					if canCheck {
+						log.Infof("%v, What do you want to do ? (raise/check/fold)", p.Players[id].Name)
+					} else {
+						log.Infof("%v, What do you want to do ? (call/raise/fold)", p.Players[id].Name)
+					}
 
-			answer = ""
+				QUESTION:
+					//read input from user
+					fmt.Scanln(&answer)
+					switch answer {
+					case "call":
+						if err := p.Call(id); err != nil {
+							log.Fatalf("%w", err)
+						}
+					case "raise":
+						if err := p.Raise(id, p.Blinds.BigBlind); err != nil {
+							log.Fatalf("%w", err)
+						}
+					case "check":
+						if !canCheck {
+							log.Info("You can't do that")
+							goto QUESTION
+						}
+						if err := p.Call(id); err != nil {
+							log.Fatalf("%w", err)
+						}
+					case "fold":
+						if err := p.Fold(id); err != nil {
+							log.Fatalf("%w", err)
+						}
+					default:
+						log.Info("I didn't understand")
+						goto QUESTION
+					}
+					answer = ""
+				}
 
-			if err := p.EndRound(); err != nil {
-				log.Fatalf("%w", err)
+				if err := p.EndRound(); err != nil {
+					log.Debugf("%w", err) //don't exit, just continue playing
+				}
+
+				if p.Round == currentRount+1 {
+					//Manages case where player 1 raised, player 0 had to call, we want to start player loop again
+					//goto NEXTROUND
+					break
+				}
 			}
 		}
 		for i := range p.Players {
@@ -152,13 +148,9 @@ func Execute() {
 		}
 		answer = ""
 
-		players := []*plays.Player{p1, p0}
-
-		p, err = plays.NewPlay(players)
-		if err != nil {
-			log.Fatalf("%w", err)
+		for id := range p.Players {
+			p.Players[id].ID = (id + 1) % len(p.Players) //move the Dealer button
 		}
-		log.Infof("BlindInfos : %v", p.Blinds)
-
+		players = []*plays.Player{p1, p0}
 	}
 }
